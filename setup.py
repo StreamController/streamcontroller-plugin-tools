67ec04fa7203266312fa0b4c861cca4fab06cdbf
Core447 <jmulmann@protonmail.com>
2024-03-24 10:30:04 +0100

    Fix: Wrong dev status

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         install_requires=["Pyro5", "loguru"],
         keywords=['python', 'StreamController', "plugin"],
         classifiers= [
-            "Development Status :: 3 - Beta",
+            "Development Status :: 4 - Beta",
             "Environment :: Plugins",
             "Intended Audience :: Developers",
             "Programming Language :: Python :: 3 :: Only",
