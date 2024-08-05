fb895f1eb01265e2db2adff263d9b22fb875bbc1
Patrick M. Niedzielski <patrick@pniedzielski.net>
2024-08-05 07:11:32 +0000

    Use `.` in place of `source` (#2)
    Python runs this command with the POSIX shell, so this patch removes
    the Bash-ism `source`.

--- streamcontroller_plugin_tools/installation_helpers.py
+++ streamcontroller_plugin_tools/installation_helpers.py
@@ -7,5 +7,5 @@ def create_venv(path: str = ".venv", path_to_requirements_txt: str = None) -> No
 
     if path_to_requirements_txt is None:
         return
-    print(f"source {join(path, 'bin', 'activate')} && pip install -r {path_to_requirements_txt}")
-    run(f"source {join(path, 'bin', 'activate')} && pip install -r {path_to_requirements_txt}", start_new_session=True, shell=True)
+    print(f". {join(path, 'bin', 'activate')} && pip install -r {path_to_requirements_txt}")
+    run(f". {join(path, 'bin', 'activate')} && pip install -r {path_to_requirements_txt}", start_new_session=True, shell=True)
